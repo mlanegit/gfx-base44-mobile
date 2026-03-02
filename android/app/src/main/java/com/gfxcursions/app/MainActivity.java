cb19ba0cbd71a0a6b063dfad37f5cde813d3c976
mlanegit <marty@gfxcursions.com>
2026-03-01 20:15:27 -0500

    Update MainActivity.java

--- android/app/src/main/java/com/gfxcursions/app/MainActivity.java
+++ android/app/src/main/java/com/gfxcursions/app/MainActivity.java
@@ -1,32 +1,5 @@
 package com.gfxcursions.app;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Handle Android hardware back button for WebView navigation
-    this.getOnBackPressedDispatcher().addCallback(this, new androidx.activity.OnBackPressedCallback(true) {
-      @Override
-      public void handleOnBackPressed() {
-        try {
-          WebView webView = (WebView) bridge.getWebView();
-          if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-          } else {
-            // No history: exit the app
-            finish();
-          }
-        } catch (Exception e) {
-          finish();
-        }
-      }
-    });
-  }
-}
+public class MainActivity extends BridgeActivity {}
